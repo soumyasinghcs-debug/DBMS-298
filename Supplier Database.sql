67a370ecac9b1013a9791be0890af42d92598abd
Soumya Singh <soumyasingh.cs24@bmsce.ac.in>
2025-11-27 09:52:33 +0530

    Update Supplier Database.sql

--- Supplier Database.sql
+++ Supplier Database.sql
@@ -64,5 +64,107 @@ AND C.sid = S.sid
 AND C.cost = (SELECT MAX (C1.cost)
 		FROM Catalog C1
 		WHERE C1.pid = P.pid);
+        
+        
+          # week 8
+          
+          
+#1
+        
+SELECT P.pid, P.pname, S.sname, C.cost
+FROM PARTS P, SUPPLIERS S, CATALOG C
+WHERE P.pid = C.pid
+AND S.sid = C.sid
+AND C.cost = (SELECT MAX(cost) FROM CATALOG);
+
+
+
+#2
+
+Select S.sname FROM SUPPLIERS S WHERE S.sid NOT IN ( SELECT C.sid FROM CATALOG C, PARTS P WHERE C.pid = P.pid AND P.color = 'Red');
+
+
+#3
+
+SELECT S.sname, SUM(C.cost) AS total_value
+FROM SUPPLIERS S, CATALOG C
+WHERE S.sid = C.sid
+GROUP BY S.sname;
+
+
+#4
+
+SELECT S.sname
+FROM SUPPLIERS S, CATALOG C
+WHERE S.sid = C.sid
+AND C.cost < 20
+GROUP BY S.sname
+HAVING COUNT(*) >= 2;
+
+#5
+
+SELECT P.pname, S.sname, C.cost
+FROM PARTS P, SUPPLIERS S, CATALOG C
+WHERE P.pid = C.pid
+AND S.sid = C.sid
+AND C.cost = (
+    SELECT MIN(C1.cost)
+    FROM CATALOG C1
+    WHERE C1.pid = P.pid);
+
+#6
+
+CREATE VIEW Supplier_Part_Count AS
+SELECT S.sid, S.sname, COUNT(C.pid) AS total_parts
+FROM SUPPLIERS S, CATALOG C
+WHERE S.sid = C.sid
+GROUP BY S.sid, S.sname;
+
+select * from Supplier_Part_Count;
+
+#7
+
+CREATE VIEW MostExpSupplier AS
+SELECT P.pid, P.pname, S.sname, C.cost
+FROM PARTS P, SUPPLIERS S, CATALOG C
+WHERE P.pid = C.pid
+AND S.sid = C.sid
+AND C.cost = (
+    SELECT MAX(C1.cost)
+    FROM CATALOG C1
+    WHERE C1.pid = P.pid
+);
+
+select * from MostExpSupplier;
+
+DELIMITER $$
+
+CREATE TRIGGER prevent_low_cost
+BEFORE INSERT ON CATALOG
+FOR EACH ROW
+BEGIN
+    IF NEW.cost < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cost cannot be below 1';
+    END IF;
+END $$
+
+DELIMITER ;    
+
+#9
+
+DELIMITER $$
+
+CREATE TRIGGER set_default_cost
+BEFORE INSERT ON CATALOG
+FOR EACH ROW
+BEGIN
+    IF NEW.cost IS NULL THEN
+        SET NEW.cost = 10;
+    END IF;
+END $$
+
+DELIMITER ;
+
 
 
